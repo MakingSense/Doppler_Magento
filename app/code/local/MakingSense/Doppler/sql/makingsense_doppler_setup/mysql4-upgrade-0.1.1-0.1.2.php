392d2bed1e767ae10f8dd2a417b4986c5282dd11
Gabriel Guarino <guarinogabriel@gmail.com>
2015-12-15 15:30:52 -0300

    Create customer attribute to track synced customers

---
+++ app/code/local/MakingSense/Doppler/sql/makingsense_doppler_setup/mysql4-upgrade-0.1.1-0.1.2.php
@@ -0,0 +1,30 @@
+<?php
+    $installer = $this;
+    $installer->startSetup();
+
+    $entityTypeId     = $installer->getEntityTypeId(Mage_Catalog_Model_Category::ENTITY);
+    $attributeSetId   = $installer->getDefaultAttributeSetId($entityTypeId);
+    $attributeGroupId = $installer->getDefaultAttributeGroupId($entityTypeId, $attributeSetId);
+
+    $installer->addAttribute('customer', 'doppler_synced', array(
+        'input'         => 'boolean',
+        'type'          => 'int',
+        'label'         => 'Synced in Doppler',
+        'visible'       => 1,
+        'required'      => 0,
+        'user_defined' => 0,
+    ));
+
+    $installer->addAttributeToGroup(
+        $entityTypeId,
+        $attributeSetId,
+        $attributeGroupId,
+        'doppler_synced',
+        '999'
+    );
+
+    $oAttribute = Mage::getSingleton('eav/config')->getAttribute('customer', 'doppler_synced');
+    $oAttribute->setData('used_in_forms', array('adminhtml_customer','customer_account_create'));
+    $oAttribute->save();
+
+    $installer->endSetup();
